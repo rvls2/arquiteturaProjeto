787f30688fe8b3207937976636be7511f68a0fe5
rvls2 <167191194+rvls2@users.noreply.github.com>
2025-07-09 07:35:39 -0300

    Add files via upload
    slt,slti,slli,srli,srai

--- design/alu.sv
+++ design/alu.sv
@@ -24,15 +24,17 @@ module alu#(
             4'b0011:        // XOR
                     ALUResult = SrcA ^ SrcB;
             4'b0100:        // SLLI
-                    ALUResult = SrcA << SrcB;
+                    ALUResult = SrcA << (SrcB & 32'h1F);
             4'b0101:        // SRLI
-                    ALUResult = SrcA >> SrcB;
+                    ALUResult = SrcA >> (SrcB & 32'h1F);
             4'b0110:        // SUB
                     ALUResult = $signed(SrcA) - $signed(SrcB);
             4'b0111:        // SRAI
-                    ALUResult = SrcA >>> SrcB;
+                    ALUResult = SrcA >>> (SrcB & 32'h1F);
             4'b1000:        // BEQ
                     ALUResult = (SrcA == SrcB) ? 1 : 0;
+            4'b1100:
+                    ALUResult = ($signed(SrcA) < $signed(SrcB)) ? 1 : 0;
             default:
                     ALUResult = 0;
             endcase
