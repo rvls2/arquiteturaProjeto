4953c4864b4095090b48b49713a777f9780f7ec0
Milla Rwana de araújo Silva <mras3@cin.ufpe.br>
2025-07-24 14:45:35 -0300

    Update Datapath.sv

--- Datapath.sv
+++ Datapath.sv
@@ -20,6 +20,7 @@ module Datapath #(
     Branch,  // Branch Enable
     input  logic [          1:0] ALUOp,
     input  logic [ALU_CC_W -1:0] ALU_CC,         // ALU Control Code ( input of the ALU )
+    input  logic                 Halt,
     output logic [          6:0] opcode,
     output logic [          6:0] Funct7,
     output logic [          2:0] Funct3,
@@ -51,11 +52,14 @@ module Datapath #(
   logic [DATA_W-1:0] FAmux_Result;
   logic [DATA_W-1:0] FBmux_Result;
   logic Reg_Stall;  //1: PC fetch same, Register not update
+  logic PC_Stall; //Parar PC
 
   if_id_reg A;
   id_ex_reg B;
   ex_mem_reg C;
   mem_wb_reg D;
+
+  assign PC_Stall = Reg_Stall || Halt; //Impedir que PC_Stall seja atualizado
 
   // next PC
   adder #(9) pcadd (
@@ -73,7 +77,7 @@ module Datapath #(
       clk,
       reset,
       Next_PC,
-      Reg_Stall,
+      PC_Stall,
       PC
   );
   instructionmemory instr_mem (
